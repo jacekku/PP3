6a88515b1091edf1b0ab2529d8ac40e907dd02d7
jacekku <kecajwy@gmail.com>
2018-11-04 10:20:15 +0100

    added ex.34

---
+++ Python/02/zadanie34.py
@@ -0,0 +1,11 @@
+names=[
+    "Genowefa",
+    "Onufry",
+    "Celestyna",
+    "Alojzy",
+    "Pankracy",
+    "Teofil",
+]
+
+longest_name=max(names,key=len)
+print(f'Najdluzsze imie: {longest_name}')
