b687f6b0161ef6696637c389ccd1be55b5f02f6d
jacekku <kecajwy@gmail.com>
2018-11-09 18:24:30 +0100

    ex 04 two lists

---
+++ Python/04/zadanie04.py
@@ -0,0 +1,46 @@
+"""
+DATA
+
+Java :      61K
+Python:     47K
+JavaScript: 37K
+C++:        32K
+C#:         26K
+Ruby:       18K
+Perl:       14K
+PHP:        14K
+C:          9K
+Android:    7K
+Graph max:  75K
+
+"""
+
+# two lists implementation as stated in the excercise
+languages=[
+    "Java",
+    "Python",
+    "JavaScript",
+    "C++",
+    "C#",
+    "Ruby",
+    "Perl",
+    "PHP",
+    "C",
+    "Android"
+]
+
+values=[
+    61,47,37,32,26,18,14,14,9,7
+]
+max_value=max(*values)
+longest_name=len(max(*languages,key=len))
+max_graph_height=40 # arbitrary
+
+for i in range(len(languages)):
+    percentage=int((values[i]/max_value)*max_graph_height)
+    print(f'{languages[i].rjust(longest_name," ")}: {"#"*percentage} {values[i]}K')
+
+
+
+
+# dictionary implementation
