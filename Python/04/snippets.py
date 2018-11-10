71a8a13df0039815da1e75bfdc65692ec437d1f6
jacekku <kecajwy@gmail.com>
2018-11-10 12:02:16 +0100

    moving the determinant function to snippets

--- Python/04/snippets.py
+++ Python/04/snippets.py
@@ -43,4 +43,42 @@ def transpose_matrix(matrix):
 
 
 
+def remove_row_col(matrix,row,col):
+    size=len(matrix)
+    temp=create_matrix(size-1,size-1)
+    temp_x,temp_y=0,0
+    for i in range(size):
+        for j in range(size):
+            if(i==row or j==col):
+                pass
+            else:
+                temp[temp_y][temp_x]=matrix[i][j]
+                temp_x+=1
+                if(temp_x==(size-1)):
+                    temp_x=0
+                    temp_y+=1
+                
+    return temp
+
+def cofactor(i,j,val,matrix):
+    #                      two function recursion
+    return ((-1)**(i+j))*val*determinant(matrix)
+
+def determinant(matrix):
+    if(len(matrix)!=len(matrix[0])):
+        raise IndexError("Matrix must be square")
+    size=len(matrix)
+    if(size==1):
+        return matrix[0][0]
+    if(size==2):
+        return matrix[0][0]*matrix[1][1] - (matrix[0][1]*matrix[1][0])
+    det=0
+    for j in range(size):
+        #two function recursion
+        det+=cofactor(  0,
+                        j,
+                        matrix[0][j],
+                        remove_row_col(matrix,0,j))
+    return det
+
 
