eafc375aa89b5a4d6103f978c1b369c472732610
jacekku <kecajwy@gmail.com>
2018-11-10 12:02:03 +0100

    ex 11 using the determinant created earlier

--- Python/04/zadanie11.py
+++ Python/04/zadanie11.py
@@ -1,53 +1,28 @@
-from snippets import create_matrix,print_matrix
-from random import randint
+from snippets import print_matrix,determinant
+
+P1=(1,1)
+P2=(2,2)
+P3=(3,3)
+
+matrix=[
+    [*P1,1],
+    [*P2,1],
+    [*P3,1],
+]
+
+print_matrix(matrix)
+print(determinant(matrix))
 
 
-def remove_row_col(matrix,row,col):
-    size=len(matrix)
-    temp=create_matrix(size-1,size-1)
-    temp_x,temp_y=0,0
-    for i in range(size):
-        for j in range(size):
-            if(i==row or j==col):
-                pass
-            else:
-                temp[temp_y][temp_x]=matrix[i][j]
-                temp_x+=1
-                if(temp_x==(size-1)):
-                    temp_x=0
-                    temp_y+=1
-                
-    return temp
+P1=(1,1)
+P2=(2,2)
+P3=(3,1)
 
+matrix=[
+    [*P1,1],
+    [*P2,1],
+    [*P3,1],
+]
 
-
-
-def cofactor(i,j,val,matrix):
-    #                      two function recursion
-    return ((-1)**(i+j))*val*determinant(matrix)
-
-def determinant(matrix):
-    if(len(matrix)!=len(matrix[0])):
-        raise IndexError("Matrix must be square")
-    size=len(matrix)
-    if(size==1):
-        return matrix[0][0]
-    if(size==2):
-        return matrix[0][0]*matrix[1][1] - (matrix[0][1]*matrix[1][0])
-    det=0
-    for j in range(size):
-        det+=cofactor(  0,
-                        j,
-                        matrix[0][j],
-                        remove_row_col(matrix,0,j))
-    return det
-
-tab=create_matrix(3,3,key=randint,key_args=[0,9])
-print_matrix(tab,format="wolfram")
-print()
-print(determinant(tab))
-
-
-
-
-
+print_matrix(matrix)
+print(determinant(matrix))
