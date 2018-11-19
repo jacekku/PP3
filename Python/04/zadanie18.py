1acb1bf2124c7a790efb17144d1e503073204442
jacekku <kecajwy@gmail.com>
2018-11-19 13:15:32 +0100

    commit message

--- Python/04/zadanie18.py
+++ Python/04/zadanie18.py
@@ -3,13 +3,14 @@ def sum_digits(number):
         return sum_digits(-number)
     if(number<10):
         return number
+    print(f'{number}:{number%10} + {number//10}')
     return number%10 + sum_digits(number//10)
 
-
-assert sum_digits(1111) is 4
-assert sum_digits(1234) is 10
-assert sum_digits(-1111) is 4
-assert sum_digits(-1234) is 10
-assert sum_digits(-100100) is 2
-assert sum_digits(9911) is 20
-assert sum_digits(-1111) is 4
+print(sum_digits(123))
+#assert sum_digits(1111) is 4
+#assert sum_digits(1234) is 10
+#assert sum_digits(-1111) is 4
+#assert sum_digits(-1234) is 10
+#assert sum_digits(-100100) is 2
+#assert sum_digits(9911) is 20
+#assert sum_digits(-1111) is 4
